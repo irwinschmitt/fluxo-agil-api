1a639444b993c1d8f7e8b228e69e56ae8ea37ead
Irwin Schmitt <irwinschmitt@gmail.com>
2023-02-11 22:46:10 -0300

    feat: create initial department data

--- app/initial_data.py
+++ app/initial_data.py
@@ -8,32 +8,57 @@ By defualt `main` create a superuser if not exists
 import asyncio
 
 from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core import config, security
 from app.core.session import async_session
-from app.models import User
+from app.models import Department, User
+
+
+async def create_superuser(session: AsyncSession):
+    result = await session.execute(
+        select(User).where(User.email == config.settings.FIRST_SUPERUSER_EMAIL)
+    )
+
+    user = result.scalars().first()
+
+    if user is None:
+        new_superuser = User(
+            email=config.settings.FIRST_SUPERUSER_EMAIL,
+            hashed_password=security.get_password_hash(
+                config.settings.FIRST_SUPERUSER_PASSWORD
+            ),
+        )
+        session.add(new_superuser)
+        await session.commit()
+
+        print("Superuser was created")
+    else:
+        print("Superuser already exists in database")
+
+
+async def create_departments(session: AsyncSession):
+    departments = await session.execute(select(Department))
+
+    if departments.scalar() is None:
+        new_department = Department(
+            sigaa_id=673, acronym="FGA", title="Faculdade do Gama"
+        )
+
+        session.add(new_department)
+        await session.commit()
+        await session.refresh(new_department)
+
+        print('"', new_department.title, '" department was created', sep="")
+
+    else:
+        print("Departments already exists")
 
 
 async def main() -> None:
-    print("Start initial data")
     async with async_session() as session:
-        result = await session.execute(
-            select(User).where(User.email == config.settings.FIRST_SUPERUSER_EMAIL)
-        )
-        user = result.scalars().first()
-
-        if user is None:
-            new_superuser = User(
-                email=config.settings.FIRST_SUPERUSER_EMAIL,
-                hashed_password=security.get_password_hash(
-                    config.settings.FIRST_SUPERUSER_PASSWORD
-                ),
-            )
-            session.add(new_superuser)
-            await session.commit()
-            print("Superuser was created")
-        else:
-            print("Superuser already exists in database")
+        await create_superuser(session)
+        await create_departments(session)
 
         print("Initial data created")
 
