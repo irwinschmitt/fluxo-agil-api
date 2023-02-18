3ca14703cf433cbaa13e4039649bb9d59e4ec6ac
Irwin Schmitt <irwinschmitt@gmail.com>
2023-02-17 22:14:37 -0300

    feat(api): create endpoints to create departments

--- app/api/endpoints/departments.py
+++ app/api/endpoints/departments.py
@@ -4,9 +4,61 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.api import deps
 from app.models import Department
+from app.schemas.requests import DepartmentCreateRequest
 from app.schemas.responses import DepartmentResponse
 
 router = APIRouter()
+
+
+@router.post("/departments", response_model=DepartmentResponse, status_code=201)
+async def create_department(
+    department: DepartmentCreateRequest,
+    session: AsyncSession = Depends(deps.get_session),
+):
+    """Create a department"""
+    result = await session.execute(
+        select(Department).where(Department.sigaa_id == department.sigaa_id)
+    )
+
+    if result.scalars().one_or_none() is not None:
+        raise HTTPException(status_code=400, detail="SIGAA ID já cadastrado")
+
+    department = Department(**department.dict())
+
+    session.add(department)
+    await session.commit()
+    await session.refresh(department)
+
+    return department
+
+
+@router.post("/departments/bulk", status_code=207)
+async def create_departments(
+    departments: list[DepartmentCreateRequest],
+    session: AsyncSession = Depends(deps.get_session),
+):
+    """Create or update departments in bulk"""
+    result = await session.execute(
+        select(Department).where(
+            Department.sigaa_id.in_([d.sigaa_id for d in departments])
+        )
+    )
+
+    db_departments = result.scalars().all()
+
+    for department in departments:
+        db_department = next(
+            (d for d in db_departments if d.sigaa_id == department.sigaa_id), None
+        )
+
+        if db_department is None:
+            db_department = Department(**department.dict())
+            session.add(db_department)
+        else:
+            db_department.acronym = department.acronym
+            db_department.title = department.title
+
+    await session.commit()
 
 
 @router.get("/departments", response_model=list[DepartmentResponse])
@@ -30,7 +82,7 @@ async def read_department(
         select(Department).where(Department.id == department_id)
     )
 
-    department = result.scalars().first()
+    department = result.scalars().one_or_none()
 
     if department is None:
         raise HTTPException(status_code=404, detail="Departamento não encontrado")
